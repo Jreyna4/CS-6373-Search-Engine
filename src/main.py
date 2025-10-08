ed6c274699b74f88128ec3c74f514f436193ea40
april <aprilflores6@yahoo.com>
2025-10-08 17:34:21 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,20 @@
 from tokenizer import tokenize_html
 from indexer import InvertedIndex
+from query_processor import boolean_query
 
-# Fake documents for now (so you don’t need Jan.zip yet)
+# Fake docs (later replace with Jan.zip files)
 docs = [
     ("doc1.html", "<html><body>cat dog cat</body></html>"),
     ("doc2.html", "<html><body>dog rat</body></html>")
 ]
 
-# Build the index
+# Build index
 index = InvertedIndex()
-
 for i, (name, html) in enumerate(docs):
-    tokens, urls = tokenize_html(html)   # use tokenizer
+    tokens, urls = tokenize_html(html)
     index.add_document(i, name, tokens, urls)
 
-# Print some checks
-print("Doc info:", index.doc_info)
-print("Index for 'cat':", index.index.get("cat"))
-print("Index for 'dog':", index.index.get("dog"))
-print("Index for 'rat':", index.index.get("rat"))
+# Try Boolean queries
+print("Q1 (cat OR dog):", boolean_query(index, ["cat", "dog"], "OR"))
+print("Q2 (cat AND dog):", boolean_query(index, ["cat", "dog"], "AND"))
+print("Q3 (cat BUT dog):", boolean_query(index, ["cat", "dog"], "BUT"))
